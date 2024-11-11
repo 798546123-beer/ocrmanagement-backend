8865fcdbf0809fba60924a204b4898e4bf330e59
Zoey <2356413947@qq.com>
2024-11-11 19:36:23 +0800

    加上component注解

--- backend/jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/system/pojo/User.java
+++ backend/jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/system/pojo/User.java
@@ -1,8 +1,12 @@
 package org.jeecg.modules.system.pojo;
 
 import lombok.Data;
+import org.jeecg.modules.system.entity.Role;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 @Data
+@Component
 public class User {
     private Integer id;
     private String username;
@@ -12,6 +16,21 @@ public class User {
     private String phone;
     private String number;
     private String type_name;
+    private Integer user_type_id;
+    @Autowired
+    private Role role;
+    //根据roleId 查一下数据库对应的角色类型名称
+public User(org.jeecg.modules.system.entity.User user){
+    this.id=user.getId();
+    this.age=user.getUser_age();
+    this.username=user.getUser_name();
+    this.password=user.getUser_pwd();
+    this.gender=user.getGender();
+    this.number=user.getUser_number();
+    this.phone=user.getUser_phone();
+    this.type_name=role.getRole_name();
+    this.user_type_id=role.getRole_id();
 
+}
 
 }
