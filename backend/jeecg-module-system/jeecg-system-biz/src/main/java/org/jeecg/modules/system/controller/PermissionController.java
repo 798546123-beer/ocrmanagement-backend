560353bbeb4e6f7db4266e5c0d0314117cdf4ee8
798546123-beer <2561285872@qq.com>
2024-12-01 19:47:41 +0800

    refactor(system): 重构权限相关代码
    - 使用 Lombok 注解简化 PageEntity 类
    - 添加 Swagger 注解和权限控制注解- 优化 PermissionController 接口文档

--- backend/jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/system/controller/PermissionController.java
+++ backend/jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/system/controller/PermissionController.java
@@ -1,6 +1,9 @@
 package org.jeecg.modules.system.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.jeecg.common.api.vo.Result;
+import org.jeecg.config.shiro.IgnoreAuth;
 import org.jeecg.modules.system.entity.PageEntity;
 import org.jeecg.modules.system.service.IPermissionService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,13 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@Api(tags = "权限相关接口")
 @RequestMapping("/system/permission")
 public class PermissionController {
 
     @Autowired
     private IPermissionService permissionService;
-
+    @IgnoreAuth
     @GetMapping("/getPermissionList")
+    @ApiOperation("权限列表获取")
     public Result<List<PageEntity>> getPermissionList() {
         List<PageEntity> permissionList = permissionService.getPermissionList();
         return Result.OK(permissionList);
--- backend/jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/system/entity/PageEntity.java
+++ backend/jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/system/entity/PageEntity.java
@@ -1,36 +1,13 @@
 package org.jeecg.modules.system.entity;
 
+import lombok.Data;
+
 import java.io.Serializable;
 
+@Data
 public class PageEntity implements Serializable {
     private static final long serialVersionUID = 1L;
-
     private String pageName;
     private String pageId;
     private Integer id;
-
-    // Getters and Setters
-    public String getPageName() {
-        return pageName;
-    }
-
-    public void setPageName(String pageName) {
-        this.pageName = pageName;
-    }
-
-    public String getPageId() {
-        return pageId;
-    }
-
-    public void setPageId(String pageId) {
-        this.pageId = pageId;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
 }
