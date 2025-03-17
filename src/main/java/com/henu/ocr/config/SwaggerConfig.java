2fd6a55695260f35f89457b0d755cc5e45b75dce
798546123-beer <2561285872@qq.com>
2025-03-17 14:15:11 +0800

    swagger配置

--- src/main/java/com/henu/ocr/config/SwaggerConfig.java
+++ src/main/java/com/henu/ocr/config/SwaggerConfig.java
@@ -1,31 +1,22 @@
-//package com.henu.ocr.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import springfox.documentation.builders.ApiInfoBuilder;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.service.ApiInfo;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//
-//@Configuration
-//public class SwaggerConfig {
-//    @Bean
-//    public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .apiInfo(apiInfo())
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("com.henu.ocr"))  // 扫描的包路径
-//                .paths(PathSelectors.any())
-//                .build();
-//    }
-//
-//    private ApiInfo apiInfo() {
-//        return new ApiInfoBuilder()
-//                .title("API 文档")
-//                .description("接口描述")
-//                .version("1.0")
-//                .build();
-//    }
-//}
+
+package com.henu.ocr.config;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("OCR API 文档")
+                        .version("1.0")
+                        .description("图像识别服务接口文档"))
+                .externalDocs(new ExternalDocumentation()
+                        .description("完整文档说明")
+                        .url("/doc.html"));
+    }
+}
