8b801d17a489c2420e2730c56671ade51576efaa
798546123-beer <2561285872@qq.com>
2025-03-30 17:44:05 +0800

    mybatisplus分页插件，用于加上返回给前端的total字段

---
+++ src/main/java/com/henu/ocr/config/MyBatisPlusConfig.java
@@ -0,0 +1,20 @@
+package com.henu.ocr.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan(basePackages = {"com.henu.ocr.mapper", "com.henu.ocr.entity"})  // 扩展Mapper扫描路径
+public class MyBatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
