7dc76df62827e4f1b1c04d91af931aea7b0d47a3
798546123-beer <2561285872@qq.com>
2025-03-30 17:43:36 +0800

    西工大接口测试文件

---
+++ src/main/java/com/henu/ocr/test/xgdApiRequestTest.java
@@ -0,0 +1,11 @@
+package com.henu.ocr.test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class xgdApiRequestTest {
+    public static void main(String[] args) {
+        String url = "https://api.xgd.ink/api/ocr/idcard";
+
+    }
+}
